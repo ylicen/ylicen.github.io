7835d763f50fd9965a095b5f6dd185148a3f0d4e
Ylicen <sokllmiller@gmail.com>
2026-04-15 15:46:20 +0800

    45

--- backend/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Hono } from 'hono';
-declare const app: Hono<import("hono/types").BlankEnv, import("hono/types").BlankSchema, "/">;
-export default app;
-//# sourceMappingURL=index.d.ts.map
